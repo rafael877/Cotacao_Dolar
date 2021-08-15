3af4e5faef0e2954cdb744fd9e6beacc76380a3d
Rafael Mendes <61762456+rafael877@users.noreply.github.com>
2021-08-15 17:02:22 -0300

    Add files via upload

---
+++ CotacaoDolar/CotacaoDolar/CotacaoDolarHG/FrmCotacaoDolar.Designer.cs
@@ -0,0 +1,249 @@
+﻿
+namespace CotacaoDolarHG
+{
+    partial class FrmCotacaoDolar
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.lblBuy = new System.Windows.Forms.Label();
+            this.lblSell = new System.Windows.Forms.Label();
+            this.lblVar = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.btnFechar = new System.Windows.Forms.Button();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.BackColor = System.Drawing.Color.Transparent;
+            this.btnSearch.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSearch.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(67)))), ((int)(((byte)(181)))), ((int)(((byte)(129)))));
+            this.btnSearch.Location = new System.Drawing.Point(188, 376);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(136, 47);
+            this.btnSearch.TabIndex = 2;
+            this.btnSearch.Text = "Pesquisar";
+            this.btnSearch.UseVisualStyleBackColor = false;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // lblBuy
+            // 
+            this.lblBuy.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(66)))), ((int)(((byte)(65)))), ((int)(((byte)(86)))));
+            this.lblBuy.Font = new System.Drawing.Font("Yu Gothic UI", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBuy.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.lblBuy.Location = new System.Drawing.Point(181, 125);
+            this.lblBuy.Name = "lblBuy";
+            this.lblBuy.Size = new System.Drawing.Size(150, 60);
+            this.lblBuy.TabIndex = 5;
+            this.lblBuy.Text = "0,0";
+            this.lblBuy.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblSell
+            // 
+            this.lblSell.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(63)))), ((int)(((byte)(84)))));
+            this.lblSell.Font = new System.Drawing.Font("Yu Gothic UI", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSell.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.lblSell.Location = new System.Drawing.Point(181, 219);
+            this.lblSell.Name = "lblSell";
+            this.lblSell.Size = new System.Drawing.Size(150, 46);
+            this.lblSell.TabIndex = 6;
+            this.lblSell.Text = "0,0";
+            this.lblSell.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblVar
+            // 
+            this.lblVar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(63)))), ((int)(((byte)(84)))));
+            this.lblVar.Font = new System.Drawing.Font("Yu Gothic UI", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblVar.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.lblVar.Location = new System.Drawing.Point(181, 293);
+            this.lblVar.Name = "lblVar";
+            this.lblVar.Size = new System.Drawing.Size(150, 46);
+            this.lblVar.TabIndex = 7;
+            this.lblVar.Text = "0,0";
+            this.lblVar.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label8
+            // 
+            this.label8.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(32)))), ((int)(((byte)(42)))));
+            this.label8.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label8.Cursor = System.Windows.Forms.Cursors.No;
+            this.label8.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.label8.Font = new System.Drawing.Font("Yu Gothic UI", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label8.Location = new System.Drawing.Point(-7, 467);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(554, 88);
+            this.label8.TabIndex = 21;
+            this.label8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnFechar
+            // 
+            this.btnFechar.BackColor = System.Drawing.Color.Transparent;
+            this.btnFechar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btnFechar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnFechar.Font = new System.Drawing.Font("Yu Gothic UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFechar.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(254)))), ((int)(((byte)(71)))), ((int)(((byte)(71)))));
+            this.btnFechar.Location = new System.Drawing.Point(12, 491);
+            this.btnFechar.Name = "btnFechar";
+            this.btnFechar.Size = new System.Drawing.Size(97, 47);
+            this.btnFechar.TabIndex = 14;
+            this.btnFechar.Text = "Fechar";
+            this.btnFechar.UseVisualStyleBackColor = false;
+            this.btnFechar.Click += new System.EventHandler(this.btnFechar_Click);
+            // 
+            // label9
+            // 
+            this.label9.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(35)))), ((int)(((byte)(45)))));
+            this.label9.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label9.Cursor = System.Windows.Forms.Cursors.No;
+            this.label9.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.label9.Font = new System.Drawing.Font("Yu Gothic UI", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label9.Location = new System.Drawing.Point(-7, 1);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(478, 86);
+            this.label9.TabIndex = 22;
+            this.label9.Text = "Dólar";
+            this.label9.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label7
+            // 
+            this.label7.BackColor = System.Drawing.Color.Transparent;
+            this.label7.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.label7.Font = new System.Drawing.Font("Yu Gothic UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label7.Location = new System.Drawing.Point(17, 149);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(124, 36);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Compra :";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label5
+            // 
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.label5.Font = new System.Drawing.Font("Yu Gothic UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label5.Location = new System.Drawing.Point(22, 229);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(109, 36);
+            this.label5.TabIndex = 17;
+            this.label5.Text = "Venda :";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label6
+            // 
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.label6.Font = new System.Drawing.Font("Yu Gothic UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label6.Location = new System.Drawing.Point(17, 303);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(129, 36);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "Variação :";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(115, 525);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 13);
+            this.label1.TabIndex = 23;
+            this.label1.Text = "Fechar o Aplicativo";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(330, 397);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(107, 26);
+            this.label2.TabIndex = 24;
+            this.label2.Text = "Pesquisar a cotação \r\ndo Dólar";
+            // 
+            // FrmCotacaoDolar
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::CotacaoDolarHG.Properties.Resources.funto_interesante_1;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(454, 550);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnFechar);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.lblVar);
+            this.Controls.Add(this.lblSell);
+            this.Controls.Add(this.lblBuy);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.label8);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FrmCotacaoDolar";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Cotação Dolar";
+            this.Load += new System.EventHandler(this.FrmCotacaoDolar_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Label lblBuy;
+        private System.Windows.Forms.Label lblSell;
+        private System.Windows.Forms.Label lblVar;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button btnFechar;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ CotacaoDolar/CotacaoDolar/CotacaoDolarHG/FrmCotacaoDolar.cs
@@ -0,0 +1,71 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CotacaoDolarHG
+{
+    public partial class FrmCotacaoDolar : Form
+    {
+        public FrmCotacaoDolar()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            string strURL = "https://api.hgbrasil.com/finance?array_limit=1&fields=only_results,USD&key=df505dbd";
+
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    var response = client.GetAsync(strURL).Result;
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var result = response.Content.ReadAsStringAsync().Result;
+
+                        Market market = JsonConvert.DeserializeObject<Market>(result);
+
+                        lblBuy.Text = string.Format(CultureInfo.GetCultureInfo("pt-BR"), "{0:C3}", market.Currency.Buy);
+                        lblSell.Text = string.Format(CultureInfo.GetCultureInfo("pt-BR"), "{0:C3}", market.Currency.Sell);
+                        lblVar.Text = string.Format(CultureInfo.GetCultureInfo("pt-BR"), "{0:P}", market.Currency.Variation / 100);
+                    }
+                    else
+                    {
+                        lblBuy.Text = "-";
+                        lblSell.Text = "-";
+                        lblVar.Text = "-";
+                    }
+                }
+                catch (Exception ex)
+                {
+                    lblBuy.Text = "-";
+                    lblSell.Text = "-";
+                    lblVar.Text = "-";
+
+                    MessageBox.Show(ex.Message);
+                }
+                
+            }
+        }
+
+        private void FrmCotacaoDolar_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnFechar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
